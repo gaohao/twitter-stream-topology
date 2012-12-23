2455fda83a2057970a3dd05bb03fccc5fd092680
mrhooray <hurui900313@gmail.com>
2012-12-23 18:14:24 -0500

    bug fix

--- src/main/java/com/mrhooray/bolts/FilterRetweetBolt.java
+++ src/main/java/com/mrhooray/bolts/FilterRetweetBolt.java
@@ -11,17 +11,20 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 public class FilterRetweetBolt extends BaseBasicBolt {
-	
+
 	private static final long serialVersionUID = 6069215708986553477L;
 
 	private static final long periodTime = 86400000; /* 24 hours */
+
 	@Override
 	public void execute(Tuple input, BasicOutputCollector collector) {
 		Status status = (Status) input.getValue(0);
 		if (status.isRetweet()) {
 			collector.emit("alltime", new Values(status.getRetweetedStatus()));
-			if ((new Date()).getTime() - status.getCreatedAt().getTime() <= periodTime) {
-				collector.emit("periodtime", new Values(status.getRetweetedStatus()));
+			if (System.currentTimeMillis()
+					- status.getRetweetedStatus().getCreatedAt().getTime() <= periodTime) {
+				collector.emit("periodtime",
+						new Values(status.getRetweetedStatus()));
 			}
 		} else {
 			return;
@@ -30,7 +33,9 @@ public class FilterRetweetBolt extends BaseBasicBolt {
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declareStream("alltime", new Fields("retweeted-status-alltime"));
-		declarer.declareStream("periodtime", new Fields("retweeted-status-periodtime"));
+		declarer.declareStream("alltime",
+				new Fields("retweeted-status-alltime"));
+		declarer.declareStream("periodtime", new Fields(
+				"retweeted-status-periodtime"));
 	}
 }
