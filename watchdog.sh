521422214abc0ae152120566e76671f5948b3d50
mrhooray <hurui900313@gmail.com>
2013-01-10 23:58:09 -0500

    bug fix

--- watchdog.sh
+++ watchdog.sh
@@ -2,7 +2,7 @@
 PROCESS='twitter-stream-topology'
 if [ `ps ax|grep -v grep|grep -ic $PROCESS` -lt 1 ]
 then
-    echo "`date` : $PROCESSFILE is not running on `hostname`!" | mail -s "$PROCESS down!" hurui900313@gmail.com
+    echo "`date`: $PROCESS is not running on `hostname`!" | mail -s "$PROCESS down!" hurui900313@gmail.com
     nohup java -jar ~/runnable/twitter-stream-topology.jar gardenhose > /dev/null &
 fi
 
